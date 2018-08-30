1a8991b94aa3ea2f6624279d7c667a3307cef923
Michael Corso <mcorso21@gmail.com>
2018-08-30 11:00:14 -0400

    Changed some project names, started working on apr api

---
+++ Dynamics Group 4 Project/DataAccessLayer/Models/Apr.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Layer.Models
+{
+    public class Apr
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Guid Key { get; set; }
+        public DateTime LastModified { get; set; }
+        public double Rate { get; set; }
+    }
+}
